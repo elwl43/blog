ca18fbb8507fcb014e769c928e6e1bbdbb49931b
elwl43 <elwl43@naver.com>
2022-05-12 17:36:12 +0900

    댓글 생성 기능 구현

--- src/main/java/com/pbj/blog/controller/ReplyController.java
+++ src/main/java/com/pbj/blog/controller/ReplyController.java
@@ -1,13 +1,34 @@
 package com.pbj.blog.controller;
 
+import com.pbj.blog.domain.Article;
+import com.pbj.blog.domain.Member;
+import com.pbj.blog.dto.reply.ReplySaveForm;
+import com.pbj.blog.service.ArticleService;
+import com.pbj.blog.service.MemberService;
 import com.pbj.blog.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class ReplyController {
 
     private final ReplyService replyService;
+    private final MemberService memberService;
+    private final ArticleService articleService;
+
+    @PostMapping("/articles/{article_id}/reply")
+    public String writeReply(@PathVariable(name = "article_id") Long articleId, ReplySaveForm replySaveForm, Principal principal){
+
+        Member findMember = memberService.findByLoginId(principal.getName());
+        Article findArticle = articleService.findById(articleId);
+
+        replyService.writeReply(replySaveForm, findMember, findArticle);
+        return "redirect:/articles/" + articleId;
+    }
 
 }
--- src/main/java/com/pbj/blog/service/ReplyService.java
+++ src/main/java/com/pbj/blog/service/ReplyService.java
@@ -1,6 +1,10 @@
 package com.pbj.blog.service;
 
 import com.pbj.blog.dao.ReplyRepository;
+import com.pbj.blog.domain.Article;
+import com.pbj.blog.domain.Member;
+import com.pbj.blog.domain.Reply;
+import com.pbj.blog.dto.reply.ReplySaveForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,5 +15,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReplyService {
 
     private final ReplyRepository replyRepository;
+
+    @Transactional
+    public void writeReply(ReplySaveForm replySaveForm, Member findMember, Article findArticle) {
+
+        Reply reply = Reply.createReply(replySaveForm);
+        reply.setMember(findMember);
+        reply.setArticle(findArticle);
+
+        replyRepository.save(reply);
+
+    }
 
 }
