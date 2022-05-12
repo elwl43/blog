64f2269943c26e8c4ae6812010aed5d3f50c79a6
elwl43 <elwl43@naver.com>
2022-05-12 20:55:47 +0900

    댓글 리스트 기능 구현

--- src/main/java/com/pbj/blog/dto/article/ArticleDTO.java
+++ src/main/java/com/pbj/blog/dto/article/ArticleDTO.java
@@ -1,11 +1,13 @@
 package com.pbj.blog.dto.article;
 
 import com.pbj.blog.domain.Article;
+import com.pbj.blog.domain.Reply;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -22,6 +24,7 @@ public class ArticleDTO {
 
     private LocalDateTime regDate;
     private LocalDateTime updateDate;
+    private List<Reply> replies;
 
     public ArticleDTO(Article article){
         this.id = article.getId();
@@ -30,7 +33,7 @@ public class ArticleDTO {
         this.nickname = article.getMember().getNickname();
         this.regDate = article.getRegDate();
         this.updateDate = article.getUpdateDate();
-
+        this.replies = article.getReplies();
     }
 
 
